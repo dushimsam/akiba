89ec1a2840f173062927b9c7e5446739d9ff70b7
DLOADIN <m.david@alustudent.com>
2026-07-03 20:42:11 +0200

    Added some new packages and an additional function to the main javascript file: index.js

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
+import path from 'path'
+import { fileURLToPath } from 'url'
 import connectDB from './db.js'
 import transactionRoutes from './routes/transactions.js'
 
@@ -9,6 +11,9 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const clientDistPath = path.resolve(__dirname, '../client/dist')
 
 // Middleware
 app.use(cors())
@@ -24,6 +29,16 @@ app.get('/api/health', (req, res) => {
 })
 
 app.use('/api/transactions', transactionRoutes)
+
+// Serve the built frontend in production containers.
+app.use(express.static(clientDistPath))
+app.get('*', (req, res, next) => {
+  if (req.path.startsWith('/api')) {
+    return next()
+  }
+
+  res.sendFile(path.join(clientDistPath, 'index.html'))
+})
 
 // Start server
 app.listen(PORT, () => {
